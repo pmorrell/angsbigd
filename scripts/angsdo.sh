4ed559c95731e6ce79f111a73ffdae3f1a8aa15f
rossibarra <rossibarra@ucdavis.edu>
2013-12-27 16:36:22 -0800

    work on merge.sh, add gitignore, mod to readme

--- scripts/angsdo.sh
+++ scripts/angsdo.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -l
-#SBATCH -D /home/jri/projects/genomesize
-#SBATCH -J jellyfish
+#SBATCH -D /home/jri/projects/bigd/angsdo/
+#SBATCH -J angsdo
 #SBATCH -o outs/out-%j.txt
-#SBATCH -p serial
+#SBATCH -p bigmem
 #SBATCH -e errors/error-%j.txt
 
-# script to run jellyfish on paul's files
+# script to run ANGSD on hapmap2 bam files
+module load angsd
 
-/home/jri/src/angsd0.570/angsd -bam TIL09_FC61694AAXX_2.ba
+angsd -do_stat outs/temp.thetas.gz -bam /group/jrigrp/hapmap2_bam/Disk3CSHL_bams_bwamem/TIL09_FC61694AAXX_2.bam -r chr1:1-100000 
---
+++ scripts/merge.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+#SBATCH -D /home/jri/projects/bigd/angsdo/
+#SBATCH -J bam_merge
+#SBATCH -o outs/out-%j.txt
+#SBATCH -p bigmem
+#SBATCH -e errors/error-%j.txt
+
+# merge bam files to single individuals 
+cd /group/jrigrp/hapmap2_bam/Disk3CSHL_bams_bwamem/
+
+for i in $( ls /group/jrigrp/hapmap2_bam/Disk3CSHL_bams_bwamem/ | cut -f 1 -d "_" | sort -n | uniq ); do
+
+#merge >1 bam files
+if [ $( ls -1 /group/jrigrp/hapmap2_bam/Disk3CSHL_bams_bwamem/$i* | wc -l ) -gt 1 ]
+then
+
+	samtools merge -r /group/jrigrp/hapmap2_bam/Disk3CSHL_bams_bwamem/"$i"_merged.bam $( ls /group/jrigrp/hapmap2_bam/Disk3CSHL_bams_bwamem/$i* | perl -ne '{BEGIN} chomp; print "$_\t"; while(<>){ chomp; print "$_\t";}; {END} chomp; print $_;')
+
+#rename if only 1
+elif [ ! -f /group/jrigrp/hapmap2_bam/Disk3CSHL_bams_bwamem/"$i"_merged.bam ]
+then
+
+	mv $( ls -1 /group/jrigrp/hapmap2_bam/Disk3CSHL_bams_bwamem/$i*  ) /group/jrigrp/hapmap2_bam/Disk3CSHL_bams_bwamem/"$i"_merged.bam;
+	echo "renamed $i!"
+
+#already renamed
+else
+	echo "$i already merged!"
+fi;
+
+done;
