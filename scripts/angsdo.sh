aca2ded651443d0e8f75a9ea7ef60b8d45179441
rossibarra <rossibarra@ucdavis.edu>
2013-12-28 00:50:52 -0800

    added indexing to merge script. appear to have initial SFS working in angsdo

--- scripts/angsdo.sh
+++ scripts/angsdo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH -D /home/jri/projects/bigd/angsdo/
+#SBATCH -D /home/jri/projects/bigd/angsbigd/
 #SBATCH -J angsdo
 #SBATCH -o outs/out-%j.txt
 #SBATCH -p bigmem
@@ -8,4 +8,13 @@
 # script to run ANGSD on hapmap2 bam files
 module load angsd
 
-angsd -do_stat outs/temp.thetas.gz -bam /group/jrigrp/hapmap2_bam/Disk3CSHL_bams_bwamem/TIL09_FC61694AAXX_2.bam -r chr1:1-100000 
+#(estimate an SFS)
+#NB this TRIP file is a temporary file based on one BAM. Do not use for real stuff.
+angsd -bam data/temp_merged_list.txt -out temp/temp_pest -doSaf 1 -uniqueOnly 1 -fold 1 -anc temp/TRIP_try.fa.gz -minMapQ 20 -minQ 1 -GL 1 -r 10:1-10000000
+
+#(calculate thetas)
+#angsd -bam data/temp_merged_list.txt -out outs/thetas -doThetas 1 -doSaf 1 -pest outs/temp_pest.saf 
+
+#(calculate Tajimas.)
+#../angsd0.551/misc/bgid make_bed bongo.thetas.gz 
+#../angsd0.551/misc/bgid do_stat bongo.thetas.gz -nChr 40
