cf7e32bbeb5bdd3c85722e5348ce89d18e98a5f2
rossibarra <rossibarra@ucdavis.edu>
2013-12-31 01:37:46 -0800

    working! TajD and thetas for chr1 hopefully coming soon.

--- scripts/angsdo.sh
+++ scripts/angsdo.sh
@@ -4,7 +4,7 @@
 #SBATCH -o outs/out-%j.txt
 #SBATCH -p bigmem
 #SBATCH -e errors/error-%j.txt
-#SBATCH -c 4 
+#SBATCH -c 8
 
 #while getopts "b:" opt; do
 #case $opt in
@@ -33,22 +33,24 @@
 module load angsd
 
 taxon=$1
+n=$( expr 2 \* $( wc -l data/"$taxon"_list.txt | cut -f 1 -d " " ))
 echo "taxon: $taxon" 1>&2
 
 #(estimate an SFS)
 #NB this TRIP file is a temporary file based on one BAM. Do not use for real stuff.
 echo "step 1 sfs" 1>&2
 
-angsd -bam data/"$taxon"_list.txt -out temp/"$taxon"_pest -doSaf 1 -uniqueOnly 1 -anc data/TRIP.fa.gz -minMapQ 20 -minQ 1 -GL 1 -r 10:1-10000000
+angsd -bam data/"$taxon"_list.txt -out temp/"$taxon"_pest -doSaf 1 -uniqueOnly 1 -anc data/TRIP.fa.gz -minMapQ 20 -minQ 1 -GL 1 -r 1: -P 8
 
 echo "step 2 em stuff" 1>&2
 # num chromes should = n for folded and 2n for unfolded
-emOptim2 temp/"$taxon"_pest.saf expr 2 \* $( wc -l data/"$taxon"_list.txt | cut -f 1 -d " " ) -nSites 10000000 > temp/"$taxon"_pest.em.ml
+emOptim2 temp/"$taxon"_pest.saf $n -nSites 10000000 -P 8 > temp/"$taxon"_pest.em.ml
+
 
 #(calculate thetas)
 echo "step 3 thetas" 1>&2
-angsd -bam data/"$taxon"_list.txt -out results/"$taxon"_thetas -doThetas 1 -doSaf 1 -GL 1 -pest temp/"$taxon"_pest.em.ml -anc data/TRIP.fa.gz -r 10:1-10000000 -win 50000 -step 10000 -P 4
+angsd -bam data/"$taxon"_list.txt -out results/"$taxon" -doThetas 1 -doSaf 1 -GL 1 -pest temp/"$taxon"_pest.em.ml -anc data/TRIP.fa.gz -r 1: -P 8
 
 #(calculate Tajimas.)
-#../angsd0.551/misc/bgid make_bed bongo.thetas.gz 
-#../angsd0.551/misc/bgid do_stat bongo.thetas.gz -nChr 40
+thetaStat make_bed results/"$taxon".thetas.gz results/$taxon 
+thetaStat do_stat results/"$taxon" -nChr $n -win 50000 -step 10000 
